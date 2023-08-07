782632c54916ab7abcb8338cb49b319df1abaf25
kaflebruh <124806580+kaflebruh@users.noreply.github.com>
2023-08-07 11:07:09 -0400

    Final Update

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# kafle-samaya-fip-final-second-sem
+## fip
+FIP
+
+## Installation
+Download the zip file in your desktop and unzip it
+
+## Usage
+Open index.html to view the website
+
+## Contributing
+Fork it!
+Create your feature branch: git checkout -b my-new-feature
+Commit your changes: git commit -am 'Add some feature'
+Push to the branch: git push origin my-new-feature
+Submit a pull request :D
+
+## Credits
+Kafle-Samaya
---
+++ js/script.js
@@ -0,0 +1,39 @@
+/*for hero image*/
+window.addEventListener('scroll', function () {
+    var body = document.querySelector('body');
+    var backgroundLogo = document.querySelector('.background-image-logo');
+  
+    if (window.scrollY > 0) {
+      body.classList.add('scroll-down');
+      backgroundLogo.classList.add('fade-out-scroll');
+    } else {
+      body.classList.remove('scroll-down');
+      backgroundLogo.classList.remove('fade-out-scroll');
+    }
+  });
+  /*promo*/
+  const promoButton = document.getElementById('promoButton');
+  const promoLightbox = document.getElementById('promoLightbox');
+  const promoContent = document.getElementById('promoContent');
+  const closeButton = document.getElementById('closeButton');
+  
+  promoButton.addEventListener('click', () => {
+      const promoDetails = [
+        /* promotion message*/
+          "Buy 1 & get 1 for absolutely free",
+      ];
+  
+      const randomPromo = promoDetails[Math.floor(Math.random() * promoDetails.length)];
+      promoContent.textContent = randomPromo;
+      
+      promoLightbox.style.display = 'flex';
+  });
+  
+  closeButton.addEventListener('click', () => {
+      promoLightbox.style.display = 'none';
+  });  
+  
+  
+  
+  
+  
